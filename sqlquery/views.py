222e5208caf48454b7ee71ae2f4217c0c1d4d6f6
juliusmarkwei <juliusmarkwei2000@gmail.com>
2024-10-09 18:17:09 +0000

    sql formatter

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -3,12 +3,19 @@ from django.db import connection
 from .models import Product
 import json
 from django.http import JsonResponse
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import SqlLexer
+from sqlparse import format
 
 def home(request):
     qs = Product.objects.all()
     serialized_data = serialize("json", qs)
 
-    print(connection.queries)
+    queries = list(connection.queries)
+    for query in queries:
+        sqlFormatted = format(str(query["sql"]), reindent=True)
+        print(highlight(sqlFormatted, SqlLexer(), TerminalFormatter()))
 
     serialized_data = json.loads(serialized_data)
     return JsonResponse(serialized_data, safe=False, status=200)
